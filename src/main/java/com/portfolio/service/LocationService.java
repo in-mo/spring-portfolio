378a4e5a00f724c127100829f3718b4b7a47622c
Mark91U <yin5946@naver.com>
2021-02-20 16:29:29 +0900

    메인화면 트랙잭션 처리

--- src/main/java/com/portfolio/service/LocationService.java
+++ src/main/java/com/portfolio/service/LocationService.java
@@ -1,12 +1,21 @@
 package com.portfolio.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.portfolio.domain.HostVo;
 import com.portfolio.domain.LocationVo;
+import com.portfolio.domain.ReviewVo;
+import com.portfolio.mapper.HostMapper;
+import com.portfolio.mapper.ImagesMapper;
 import com.portfolio.mapper.LocationMapper;
+import com.portfolio.mapper.ReviewMapper;
 
 @Service
 public class LocationService {
@@ -14,7 +23,35 @@ public class LocationService {
 	@Autowired
 	private LocationMapper locationMapper;
 	
-	public List<LocationVo> getLocationList() {
-		return locationMapper.getLocationList();
+	@Autowired
+	private ReviewMapper reviewMapper;
+	
+	@Autowired
+	private HostMapper hostMapper;
+	
+	@Autowired
+	private ImagesMapper imagesMapper;
+	
+	@Transactional
+	public Map<String, Object> getLocationListAndMainInfo() {
+
+		List<LocationVo> locationList = locationMapper.getLocationList();
+		List<ReviewVo> reviewList = reviewMapper.getReviews();
+		List<HostVo> hostList = new ArrayList<HostVo>();
+		hostList.add(hostMapper.getContentInfoForMain("아파트"));
+		hostList.add(hostMapper.getContentInfoForMain("주택"));
+		hostList.add(hostMapper.getContentInfoForMain("독특한 숙소"));
+		hostList.add(hostMapper.getContentInfoForMain("부티크 호텔"));
+		
+		for(HostVo hostVo : hostList) {
+			hostVo.setImageVo(imagesMapper.getImageByNoNum(hostVo.getNum()));
+		}
+		
+		
+		Map<String, Object> obj = new HashMap<>();
+		obj.put("locationList", locationList);
+		obj.put("reviewList", reviewList);
+		obj.put("hostList", hostList);
+		return obj;
 	}
 }
